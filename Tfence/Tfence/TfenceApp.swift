e564f8a7e0f7cfe1b9fd080d4c3066a1744c381f
sun <dkdlshs123@naver.com>
2025-10-19 15:00:54 +0900

    update: init 프로젝트

---
+++ Tfence/Tfence/TfenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TfenceApp.swift
+//  Tfence
+//
+//  Created by vision on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TfenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
